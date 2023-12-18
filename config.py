c244899401d3ad21840ac443dda6e552139b40a6
Limieon <justin.jakob391@outlook.com>
2023-12-18 20:56:51 +0000

    Added more config values

--- config.py
+++ config.py
@@ -2,11 +2,13 @@
 
 class SparkUIConfig:
     class API:
-        HOST = "0.0.0.0"
-        PORT = 1910
+        HOST = "0.0.0.0"                                # The host to bind the API server on
+        PORT = 1910                                     # The port to expose the API server to
     class Directories:
         class StableDiffusion:
-            CHECKPOINT = "assets/sd/checkpoint"
-            LORA = "assets/sd/lora"
-            EMBEDDING = "assets/sd/embedding"
-            CONTROL_NET = "assets/sd/control_net"
+            CHECKPOINT = "assets/sd/checkpoint"         # The checkpoint asset dir
+            LORA = "assets/sd/lora"                     # The lora asset dir 
+            EMBEDDING = "assets/sd/embedding"           # The embedding asset dir
+            CONTROL_NET = "assets/sd/control_net"       # The controlnet root dir
+            VAE = "assets/sd/vae"                       # The vae asset dir
+            IMAGES_OUT = "assets/out/images"            # The image out dir
