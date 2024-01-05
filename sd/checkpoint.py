9d65703a1b626c8a9cae038ab3d71f88e808c6e9
Justin Jakob <justin.jakob391@gmail.com>
2024-01-05 18:51:15 +0100

    Initialized Cargo

--- sd/checkpoint.py
+++
@@ -1,7 +0,0 @@
-from fastapi import UploadFile
-
-from config import SparkUIConfig as Config
-from utils import upload_file
-
-async def upload_checkpoint(file: UploadFile):
-    await upload_file(Config.StableDiffusion.Directories.CHECKPOINT, file)
