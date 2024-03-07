24b8a9a5e9b0ce6c29e00262c6de63806a15b315
Justin Jakob <justin.jakob391@gmail.com>
2024-03-08 00:26:57 +0100

    Putted pipeline manager into own file

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from stable_diffusion import StableDiffusionHandler, StableDiffusionBaseModel
+from stable_diffusion import StableDiffusionBaseModel
+from stable_diffusion.pipeline_manager import PipelineManager
 
 
 def main():
-    sd_handler = StableDiffusionHandler()
+    pipelines = PipelineManager()
 
     # Prompt Credit: https://civitai.com/images/7260553
     prompt = "anime, girl, wizard hat, robe, thighhighs, close-up, happy, magic, fire, bokeh, depth of field, transparent, light particles, bloom effect"
-    sd_handler.load_pipeline("./assets/models/StableDiffusion/bluePencilXL_v500.safetensors", StableDiffusionBaseModel.SDXL1_0)(
+    pipelines.load_pipeline("./assets/models/StableDiffusion/bluePencilXL_v500.safetensors", StableDiffusionBaseModel.SDXL1_0)(
         prompt, num_inference_steps=20, guidance_scale=5
     ).images[0].save("assets/outputs/image.png")
 
-    sd_handler.load_pipeline("./assets/models/StableDiffusion/bluePencilXL_v500.safetensors", StableDiffusionBaseModel.SDXL1_0)(
+    pipelines.load_pipeline("./assets/models/StableDiffusion/bluePencilXL_v500.safetensors", StableDiffusionBaseModel.SDXL1_0)(
         prompt, num_inference_steps=20, width=576, height=1024, guidance_scale=5
     ).images[0].save("assets/outputs/image2.png")
 
--- stable_diffusion/__init__.py
+++ stable_diffusion/__init__.py
@@ -1,13 +1,6 @@
 from enum import Enum
 from dataclasses import dataclass
 
-import torch
-
-from diffusers import (
-    DiffusionPipeline,
-    StableDiffusionPipeline,
-    StableDiffusionXLPipeline,
-)
 
 from hash_utils import get_sha256
 
@@ -18,42 +11,3 @@ class StableDiffusionBaseModel(Enum):
     SDXL1_0 = 10
     SDXL1_0Turbo = 20
     SDXL1_0Lightning = 30
-
-
-class StableDiffusionHandler:
-    def get_model_hash(self, path: str):
-        if not path in self.model_hashes:
-            self.model_hashes[path] = get_sha256(path)
-
-        return self.model_hashes[path]
-
-    def load_pipeline(self, path: str, base: StableDiffusionBaseModel):
-        hash = self.get_model_hash(path)
-
-        if not hash in self.pipelines:
-            print(f"Model '{path}' not cached! Loading...")
-
-            if base == StableDiffusionBaseModel.SD1_5 or base == StableDiffusionBaseModel.SD2_1:
-                pipeline = StableDiffusionPipeline.from_single_file(path, torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
-                pipeline.to("cuda")
-
-                pipeline.enable_model_cpu_offload()
-                pipeline.enable_xformers_memory_efficient_attention()
-
-                self.pipelines[hash] = pipeline
-
-            if base == StableDiffusionBaseModel.SDXL1_0 or base == StableDiffusionBaseModel.SDXL1_0Turbo or base == StableDiffusionBaseModel.SDXL1_0Lightning:
-                pipeline = StableDiffusionXLPipeline.from_single_file(path, torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
-                pipeline.to("cuda")
-
-                pipeline.enable_model_cpu_offload()
-                pipeline.enable_xformers_memory_efficient_attention()
-
-                self.pipelines[hash] = pipeline
-
-            print("Done!")
-
-        return self.pipelines[hash]
-
-    model_hashes: dict[str, str] = {}
-    pipelines: dict[str, DiffusionPipeline] = {}
