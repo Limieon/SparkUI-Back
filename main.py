7057be60ed66862b05e713e521d2e1017841a313
Justin Jakob <justin.jakob391@gmail.com>
2024-03-10 17:43:38 +0100

    Added config file

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from fastapi import FastAPI
 from stable_diffusion import StableDiffusionBaseModel
 from stable_diffusion.pipeline_manager import PipelineManager
 
+from config import SparkConfig
+
 
 async def main():
     import api
     import spark
 
-    await api.serve(spark.app, "0.0.0.0", 1911)
+    await api.serve(spark.app, SparkConfig.API.host, SparkConfig.API.port)
 
 
 if __name__ == "__main__":
