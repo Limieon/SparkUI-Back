9028be2af9a745fd59bc799165610d60bd015d05
Limieon <justin.jakob391@outlook.com>
2024-03-11 13:59:39 +0000

    Added arg checking

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import sys
 
 from fastapi import FastAPI
 
@@ -12,6 +13,17 @@ load_dotenv()
 
 
 async def main():
+    if "--import" in sys.argv:
+        await import_models()
+
+    await start_inference_server()
+
+
+async def import_models():
+    print("Importing new models...")
+
+
+async def start_inference_server():
     import api
     import spark
 
